8e8f4f444dc7aa5907c77ec202d32a37304b11f6
Ezequiel Grigolatto <ezegrigolatto@gmail.com>
2025-10-01 17:17:52 -0300

    force revalidation

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async headers () {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
+  reactStrictMode: true,
   images: {
     domains: ['cdn.sanity.io'],
   },
