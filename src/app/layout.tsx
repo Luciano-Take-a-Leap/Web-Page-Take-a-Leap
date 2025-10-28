a8d5212251a61c03a1efcfcdca8f8070ab100037
Ezequiel Grigolatto <ezegrigolatto@gmail.com>
2025-10-28 19:45:28 -0300

    updated gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -94,12 +94,12 @@ export default async function Layout({
       <head>
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','G-EPPTYFKS6Y');
-          `}
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T56D786M');
+  `}
         </Script>
         <link rel="icon" href="/assets/favicon.jpeg" />
         <meta name="theme-color" content="#ffffff" />
@@ -141,7 +141,7 @@ export default async function Layout({
       >
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=G-EPPTYFKS6Y"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T56D786M"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
